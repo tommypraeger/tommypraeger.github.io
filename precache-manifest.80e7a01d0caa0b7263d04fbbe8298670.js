a3c81540c0c653edd7aa8e49e80d6769da719515
tommypraeger <thomaspraeger2020@u.northwestern.edu>
2019-09-24 01:04:00 -0500

    Updates

---
+++ precache-manifest.80e7a01d0caa0b7263d04fbbe8298670.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d0be9e58e2c6d3417de80de48a819b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4715b5eced60ce2721a",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "0848b43add75f2fe7e60",
+    "url": "/static/js/2.2f515186.chunk.js"
+  },
+  {
+    "revision": "c4715b5eced60ce2721a",
+    "url": "/static/js/main.62f6642f.chunk.js"
+  },
+  {
+    "revision": "34d864e0531077c1d85d",
+    "url": "/static/js/runtime-main.4cb56511.js"
+  }
+]);
